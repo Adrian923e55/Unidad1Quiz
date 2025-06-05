5fb6fca36f7f385e2e1f38ddc766f17703816a68
211G0357 <211G0357@rcarbonifera.tecnm.mx>
2025-06-05 13:45:06 -0600

    s

--- QuizCliente/Views/JuegoPrincipal.xaml.cs
+++ QuizCliente/Views/JuegoPrincipal.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuizCliente.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,7 +22,12 @@ namespace QuizCliente.Views
     {
         public JuegoPrincipal()
         {
+         
             InitializeComponent();
+            DataContext = new JuegoPrincipalViewModel(); // ⬅️ Esto enlaza la vista con el ViewModel
+       
         }
+
+        
     }
 }
